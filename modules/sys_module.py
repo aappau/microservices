b2d3ce17d2c2e5aa7e3fc6c88ba557af36a15027
aappau <tony.appau@gmail.com>
2022-02-17 21:55:35 -0500

    working with sys module

---
+++ modules/sys_module.py
@@ -0,0 +1,17 @@
+"""
+Python sys module
+ - Used to manipulate Python runtime environment
+ - Not used for operating system
+"""
+
+
+import sys
+
+print(sys.version)
+print(sys.platform)
+print(sys.path)
+
+print(sys.argv)
+
+sys.exit()
+
